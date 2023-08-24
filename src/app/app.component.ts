dddeeffb36f19f52d348f9e00b8516e9137b4b39
Aveen <Aveenkhan123@gmail.com>
2023-08-24 10:19:48 +0500

    DashboardUpdated-Aveen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     // this.router.navigate(['/']);
-    this.router.navigate(['/postdetailspage']);
+    this.router.navigate(['/dashboardpage']);
   }
 
 }
--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -7,23 +7,29 @@ import {Router} from '@angular/router';
   styleUrls: ['./dashboard-page.component.css']
 })
 export class DashboardPageComponent {
+ //Properties
+ images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+ productDescription: string = ' Stay in the loop with notifications, track your fitness goals, and even make payments right from your wrist. With its stylish design and advanced features, our smartwatch is the perfect blend of fashion and functionality. Elevate your lifestyle today!';
 
+ //constructor
+ constructor(private router: Router){
+ }
 
-  //constructor
-  constructor(private router: Router){
+ //Function to go to profile
+ gotoProfile() : void{
+   console.log('Going to my profile page....');
+   this.router.navigate(['/myprofilepage']);
+ }
 
-  }
+ //Function to go to product description
+ goToDetails() : void {
+   console.log('Going to my profile page....');
+   this.router.navigate(['/myprofilepage']);
+ }
 
-  //Function to go to profile
-  gotoProfile() : void{
-    console.log('Going to my profile page....');
-    this.router.navigate(['/myprofilepage']);
-  }
-
-  //Function to go to new post page
-  gotoNewPost() : void{
-    console.log('Going to create new post page....');
-    this.router.navigate(['/newpostpage']);
-  }
-
+ //Function to go to new post page
+ gotoNewPost() : void{
+   console.log('Going to create new post page....');
+   this.router.navigate(['/newpostpage']);
+ }
 }
